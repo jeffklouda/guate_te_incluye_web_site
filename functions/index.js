d4eb93deccc390230148bd42868eacef963bd416
Jeff Klouda <jklouda1@nd.edu>
2018-11-04 17:52:00 -0500

    alter getJobs function to iterate through all jobs

--- functions/index.js
+++ functions/index.js
@@ -4,19 +4,24 @@ admin.initializeApp(functions.config().firebase);
 
 exports.getJobs = functions.https.onRequest((request, response) => {
     const path = request.path.split('/');
-	
+    var html = "";
+    html += "<!DOCTYPE html>"
+    html += "<html>";
+    html += "<head>";
+    html += "<title>Job Page</title>";
+    html += "</head>";
+    html += "<body>";
+    html += "<h1>Job Page</h1>";
 	//var storage = firebase.storage();
-    return admin.database().ref('Data/Jobs/').once('value', (snapshot) => {
-        var jobs = snapshot.val();
-        var html = "";
-        html += "<!DOCTYPE html>"
-        html += "<html>";
-        html += "<head>";
-        html += "<title>Job Page</title>";
-        html += "</head>";
-        html += "<body>";
-        html += "<h1>Job Page</h1>";
-        html += `${jobs.job1.address}`;
+    return admin.database().ref('Data/Jobs/').once('value').then((snapshot) => {
+        snapshot.forEach(function(childSnapshot) {
+            var job_id = childSnapshot.key;
+            var job_info = childSnapshot.val();
+            html += `<p>${job_id}</p>`;
+            html += `<p>${job_info.address}</p>`;
+            html += `<p>${job_info.company}</p>`;
+            html += `<p>${job_info.title}</p>`;
+        });
         html += "</body>";
         html += "</html>";
         response.send(html);
